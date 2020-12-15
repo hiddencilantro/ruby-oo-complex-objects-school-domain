10b5c6e2e700f3817336dfe7c4047aee7a0c2f45
hiddencilantro <stevekim1109@gmail.com>
2020-12-15 10:29:43 -0500

    refactored add_student

--- lib/school.rb
+++ lib/school.rb
@@ -7,9 +7,7 @@ class School
     end
 
     def add_student(name, grade)
-        if @roster[grade] == nil
-            @roster[grade] = []
-        end
+        @roster[grade] = [] if !@roster[grade]
         @roster[grade] << name
     end
  
